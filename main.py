fdbec8c2e2f5dde80dc6752b2d6faf3fc341345c
中岡黎 <reideeplearning@gmail.com>
2019-12-18 10:07:39 +0900

    new commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -12,10 +13,13 @@ from linebot.models import (
 import os
 
 app = Flask(__name__)
+scheduler = BlockingScheduler()
 
 #環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+API_KEY = os.environ['API_KEY']
+USER_ID = os.environ['USER_ID']
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
@@ -43,6 +47,19 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
+
+
+def my_job():
+    messages = TextSendMessage(text="これはテストです")
+    line_bot_api.push_message(USER_ID, messages=messages)
+
+
+#scheduler.add_job(
+#    my_job, 'interval', start_time="2019-12-18 18:30:00", hours=24)
+scheduler.add_job(
+    my_job, 'interval', seconds=30)
+
+scheduler.start()
 
 
 if __name__ == "__main__":
